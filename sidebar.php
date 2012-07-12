646352eaa209e5185a2a0421e54f1484279859a6
Mark Kirby <mark@thisishatch.co.uk>
2012-07-12 12:22:48 +0100

    Removed broken elements in sidebar

--- sidebar.php
+++ sidebar.php
@@ -8,10 +8,7 @@
 
 	if ( function_exists('register_sidebar') )
 	register_sidebar();?>
-   <h2>Currently</h2>
-   <p><a href="http://www.thenetawards.com/"><img src="http://mark-kirby.co.uk/wp-content/uploads/2010/09/ICON_judge_web.png"></img></a></p>
 	<h2>I support</h2>
-	<p><a href="http://www.democracynow.org"><img src="http://images.democracynow.org/images/linkbanner143x27b.gif"></img></a></p>
    <p><a href='https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/String' title='JavaScript String .split'><img src='http://static.jsconf.us/promotejsh.gif' height='150' width='180' alt='JavaScript String .split'/></a></p>
 </section>
 
